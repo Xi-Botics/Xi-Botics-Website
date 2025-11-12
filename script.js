5aed56d593eeeea32466f561c87891c6e5f5f392
jkim0617 <jonkim0617@gmail.com>
2025-11-12 15:08:29 -0800

    added carousel and contact form page

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const menuBurger = document.getElementById("menu-burger");
-const menuExtend = document.getElementById("menu-extend");
-let isMenuExtended = false;
+// Source - https://stackoverflow.com/a
+// Posted by Bryan Rieger, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-11-10, License - CC BY-SA 4.0
 
-menuBurger.addEventListener('click', () => {
-  if (isMenuExtended) {
-    menuExtend.style.display = "none"
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+console.log(width);
+
+var position = 0;
+
+function moveCarousel(direction) {
+  if (direction == 'forward') {
+    if (position == 2) {
+      return false;
+    }
+    position++;
+  }
+  else if (direction == 'backward') {
+    if (position == 0) {
+      return false;
+    }
+    position--;
+  }
+  let offset = 0;
+  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
+  // if (width >= 1024) {
+  //   offset = 806 * position;
+  // }
+  if (width >= 768) {
+    offset = 406 * position;
   }
   else {
-    menuExtend.style.display = "flex"
+    offset = 306 * position;
   }
-  isMenuExtended = !isMenuExtended;
-})
-
-const logo = document.getElementById("logo")
+  document.querySelectorAll('.carousel-item').forEach(carouselItemElement => {
+    carouselItemElement.style.transform = `translateX(-${offset}px)`;
+  })
+}
